0f9c7144dbe6a74db36edd0f285f3ae85bc053c3
Ellen Lau <ellenlau@umd.edu>
2013-08-15 08:59:18 -0300

    added filtering to compute_erp.m

--- compute_erp.m
+++ compute_erp.m
@@ -1,9 +1,9 @@
 %%%Input re-referenced, epoched file with artifact rejection applied
 
 %%Example
-%%compute_erp('MOOSE','Moose',[1:18])
+%%compute_erp('MOOSE','Moose',[1:18],40)
 
-function compute_erp(expDir, expName, subjNumList)
+function compute_erp(expDir, expName, subjNumList, lpfilt)
 
 
 filePath = strcat('/Users/Shared/Experiments/',expDir,'/data/');
@@ -15,6 +15,7 @@ for subjNum = subjNumList
     
     subjFileName = strcat('EEGLAB_sets/',expName, '_S', int2str(subjNum), '_elist_bins_be_ar.set')
     outFileName = strcat('ERPs/',expName, '_S', int2str(subjNum), '.erp')
+    outFileNameFilt = strcat('ERPs/',expName, '_S', int2str(subjNum),'_lp',int2str(lpfilt),'.erp')
     
     %%loading data
     EEG = pop_loadset('filename',subjFileName,'filepath',filePath);
@@ -24,7 +25,11 @@ for subjNum = subjNumList
     %%compute erp
     ERP = pop_averager( ALLEEG , 'Criterion', 'good', 'DSindex',1, 'Stdev', 'on', 'Warning', 'on' );
     ERP = pop_savemyerp(ERP, 'erpname', strcat('S', int2str(subjNum)), 'filename', outFileName, 'filepath', filePath, 'warning', 'on');         
-    
+
+    %%filter erp
+    ERP = pop_filterp( ERP,1:32 , 'Cutoff',lpfilt, 'Design', 'butter', 'Filter', 'lowpass', 'Order',2 );
+    ERP = pop_savemyerp(ERP, 'erpname', strcat('S', int2str(subjNum)), 'filename', outFileNameFilt, 'filepath', filePath, 'warning', 'on');         
+
     %%delete epoch data
     ALLEEG = pop_delset( ALLEEG, [1] );
 
