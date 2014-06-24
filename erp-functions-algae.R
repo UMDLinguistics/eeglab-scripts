949d54b95f74ef630ce8f3daf5880cf9453e5f27
Ellen Lau <ellenlau@umd.edu>
2014-06-24 14:59:38 -0400

    unknown changes to R functions

--- erp-functions-algae.R
+++ erp-functions-algae.R
@@ -29,9 +29,15 @@ avgWinERP <- function(erpData, subjNum, condNum, epBeg, epEnd, sampleRate, t1, t
 
 #This function calls the previous two functions to create a dataframe containing mean over selected
 #time-window for all subjects and conditions of interest
-#ex: allData.df <- allSubjCondWin('MOOSE','Moose',c(1:4,6:29,31,33:36), c(1,2), -100, 798, 500, 300,500)
+#ex: allData.df <- allSubjCondWin('MOOSE','Moose','MOOSE_n36', c(1,2), -100, 798, 500, 300,500)
 
-allSubjCondWin <- function(expDir, expName, subjList, condList, epBeg, epEnd, sampleRate, t1, t2){
+allSubjCondWin <- function(expDir, expName, subjListFileID, condList, epBeg, epEnd, sampleRate, t1, t2){
+  #Read in subject list
+  filePath <- paste('/Users/Shared/Experiments/',expDir,'/results/R/',sep="")
+  fileName <- paste(filePath,'subjLists/',subjListFileID, '.txt',sep="")
+  subjList = scan(file=fileName,flush=TRUE)
+  
+  #Read in ERP data
   for (subj in subjList){
     for (cond in condList){
       if(exists('tempERP')){
@@ -56,9 +62,13 @@ allSubjCondWin <- function(expDir, expName, subjList, condList, epBeg, epEnd, sa
 #results go to a directory you need to create: '/Users/Shared/Experiments/expDir/results/R'
 #Ex:simple_2cond_ANOVA('MOOSE','Moose',c(1:4,6:29,31,33:39), 1, 2, -100, 798, 500, 300, 500)
 
-simple_2cond_ANOVA <- function(expDir, expName, subjList,c1,c2,epBeg, epEnd, sampleRate, t1,t2){
-  source('/Users/Shared/EEGLAB_scripts/erp-functions-algae.R')
-  allData.df <- allSubjCondWin(expDir,expName,subjList, c(c1,c2), epBeg, epEnd, sampleRate,t1,t2)
+simple_2cond_ANOVA <- function(expDir, expName, subjListFileID,c1,c2,epBeg, epEnd, sampleRate, t1,t2){
+  #Read in subject list
+  filePath <- paste('/Users/Shared/Experiments/',expDir,'/results/R/',sep="")
+  fileName <- paste(filePath,'subjLists/',subjListFileID, '.txt',sep="")
+  subjList = scan(file=fileName,flush=TRUE)
+  
+  allData.df <- allSubjCondWin(expDir,expName,subjListFileID, c(c1,c2), epBeg, epEnd, sampleRate,t1,t2)
 
   #exclude veog, heog, right mastoid, fp1
   allData.df <- subset(allData.df, elec != 28 & elec != 30 & elec != 31 & elec != 32)
@@ -77,8 +87,54 @@ simple_2cond_ANOVA <- function(expDir, expName, subjList,c1,c2,epBeg, epEnd, sam
   
 }
 
+##quads should be double-checked--this analysis includes 20 electrodes
+ezANOVAquad <- function(expDir, expName, subjListFileID,c1,c2,epBeg, epEnd, sampleRate, t1,t2,slabel){
+  #Read in subject list
+  filePath <- paste('/Users/Shared/Experiments/',expDir,'/results/R/',sep="")
+  fileName <- paste(filePath,'subjLists/',subjListFileID, '.txt',sep="")
+  subjList = scan(file=fileName,flush=TRUE)
+  
+  allData.df <- allSubjCondWin(expDir,expName,subjListFileID, c(c1,c2), epBeg, epEnd, sampleRate,t1,t2)
+  
+  allData.df <- subset(allData.df, elec != 28 & elec != 30 & elec != 31 & elec != 32)
+  droplevels(allData.df)
+  
+  allData.df$hem <- factor(allData.df$elec)
+  levels(allData.df$hem) <- c("R","M","L","M","R","R","R","R","R","R","L","L","L","M","M","R","R","L","L","M","M","R","R","L","L","L","L","L")
+  
+  allData.df$ap <- factor(allData.df$elec)
+  levels(allData.df$ap) <- c("X","X","X","P","P","P","P","M","P","M","P","P","P","P","M","A","A","P","M","A","A","A","A","M","A","A","A","A")
+  
+  quadData.df <- subset(allData.df, ap !="X" & ap !="M" & hem !="M")
+  quadData.df <- droplevels(quadData.df)
+  
+  c1c2Data.df <- subset(quadData.df,cond==c1 | cond==c2)
+  c1c2Data.df <- droplevels(c1c2Data.df)
+
+  
+  library('ez')
+  
+  ez_c1c2 <- ezANOVA(data=c1c2Data.df,dv=.(amp),wid=.(subj),within=.(cond,hem,ap),type=3, detailed=TRUE)
+  
+  filePath <- paste('/Users/Shared/Experiments/',expDir,'/results/R/',sep="")
+  outFileName <- paste(filePath,'anova_quad_c',c1,'c',c2,'_',t1,t2,'.txt',sep="")
+  sink(outFileName)
+  print(ez_c1c2)
+  c1c2.aov = aov(amp ~ cond * hem * ap + Error(subj/(cond * hem * ap)), data = c1c2Data.df)
+  print(model.tables(c1c2.aov,"means"), digits = 5)
+  sink()
+  
+  
+}
+
+
 #elecLabelList in grandAvgElec should be triple-checked to verify it is correct
-grandAvgElec <- function(expDir, expName, subjList, condList, elec){
+grandAvgElec <- function(expDir, expName, subjListFileID, condList, elec){
+  #Read in subject list
+  filePath <- paste('/Users/Shared/Experiments/',expDir,'/results/R/',sep="")
+  fileName <- paste(filePath,'subjLists/',subjListFileID, '.txt',sep="")
+  subjList = scan(file=fileName,flush=TRUE)
+  
   elecLabelList <- c('O2','O1','Oz','Pz','P4','CP4','P8','C4','TP8','T8','P7','P3','CP3','CPz','Cz','FC4','FT8','TP7','C3','FCz','Fz','F4','F8','T7','FT7','FC3','F3','RM','F7','FP1')
   elecNum <- which(elecLabelList == elec)
   for (subj in subjList){
@@ -97,129 +153,4 @@ grandAvgElec <- function(expDir, expName, subjList, condList, elec){
   }
   gaData <- rowMeans(allDataElec)
   return(gaData)  
-}
-
-#Core grand-avg waveform plotting function called by scripts below
-plotBasicERP <- function(expDir, expName, subjListFileID, condList, epBeg, epEnd, sampleRate, elec,col.scheme){
-  filePath <- paste('/Users/Shared/Experiments/',expDir,'/results/R/',sep="")
-  fileName <- paste(filePath,subjListFileID, '.txt',sep="")
-  subjList = scan(file=fileName,flush=TRUE)
-  time_vector <- seq(epBeg,epEnd,1000/sampleRate)
-  col.scheme = c("black","red","blue","green","cyan","orange","brown")
-  
-  firstCond <- condList[1]
-  ga_c1 <- grandAvgElec(expDir, expName,subjList,condList[1],elec)
-  plot(time_vector, ga_c1, type="l", ylim=c(5,-5), col=col.scheme[1],ylab="",xlab="",xaxt="n",yaxt="n",lwd=4, bty="n")
-  axis(1,labels=FALSE,pos=c(0,0),lwd=1,tcl=-.5,at=seq(epBeg,epEnd+5,100))
-  axis(2,labels=TRUE,pos=c(0,0),las=1,lwd=1,tcl=-.5,at=c(-2,2))
-  mtext(elec,side=2,adj=1.5,cex=1,las=1,at=-4)
-  
-  if(length(condList) > 1){
-    otherConds <-condList[2:length(condList)]    
-    count = 0  
-    for (cond in otherConds){
-      count = count + 1
-      ga_otherCond <- grandAvgElec(expDir,expName,subjList,otherConds[count],elec)
-      lines(time_vector, ga_otherCond, col=col.scheme[count+1],lwd=4)
-    }
-  }
-  
-}
-
-#This function plots the waveform for a single electrode in R, with option to export to pdf
-#Files needed: 
-##(1)ERP files for each subj/cond exported from Matlab, as described above
-##(2)Subject list text file in 'Experiments/expDir/results/R/' that has one subject number per row
-##(3)To export pdfs, need to create a directory 'Experiments/expDir/results/R/figures/'
-
-#EX: plotERP.1e('MOOSE', 'MOOSE', 'MOOSE_n33', c(1,2,3, 4), c('curly hair (hi-con hi-prob)', 'soaked hair', 'curly leaf (hi-con lo-prob)', 'crunchy leaf'),-100, 798, 500, 6,'yes')
-
-#!!elecLabelList in grandAvgElec should be triple-checked to verify it is correct
-plotERP.1e <- function(expDir, expName, subjListFileID, condList, condLabelList,epBeg, epEnd, sampleRate, elec,pdfQ){
-  colScheme = c("black","red","blue","green","cyan","orange","brown")
-  filePath <- paste('/Users/Shared/Experiments/',expDir,'/results/R/',sep="")
-  par(mfrow=c(1,1))
-  
-  if(pdfQ=='yes'){pdf(file=paste(filePath,'figures/ERP_',elec,'_',subjListFileID,'.pdf',sep=""))}
-  plotBasicERP(expDir, expName, subjListFileID, condList, epBeg, epEnd, sampleRate, elec,colScheme)
-  #mtext("μV",side=2,adj=-3, line=1, cex=1, las=1, at=4)  ##mu symbol won't encode correctly in pdf
-  mtext(subjListFileID,side=3,adj=.5,cex=1.5)
-  legend(100, 4, condLabelList, col=colScheme[1:length(condList)], lty=c(1,1), merge=TRUE,lwd=2.5,bty="n")
-  if(pdfQ=='yes'){dev.off()}
-}
-
-plotERP.4e <- function(expDir, expName, subjListFileID, condList, condLabelList,epBeg, epEnd, sampleRate, elecList,pdfQ){
-  colScheme = c("black","red","blue","green","cyan","orange","brown")
-  filePath <- paste('/Users/Shared/Experiments/',expDir,'/results/R/',sep="")
-  
-  if(pdfQ=='yes'){pdf(file=paste(filePath,'figures/ERP_test4elec_',subjListFileID,'.pdf',sep=""))}
-  par(mfrow=c(5,5))
-  for(elec in elecList){
-    plotBasicERP(expDir, expName, subjListFileID, condList, epBeg, epEnd, sampleRate, elec,colScheme)
-    #mtext("μV",side=2,adj=-3, line=1, cex=1, las=1, at=4)  ##mu symbol won't encode correctly in pdf
-  }
-  #mtext(subjListFileID,side=3,adj=.5,cex=1.5)
-  #legend(-100, 6, condLabelList, col=colScheme[1:length(condList)], lty=c(1,1), merge=TRUE,lwd=2.5,bty="n")
-  if(pdfQ=='yes'){dev.off()}
-}
-
-plotERP.9e <- function(expDir, expName, subjListFileID, condList, condLabelList,epBeg, epEnd, sampleRate, pdfQ){
-  colScheme = c("black","red","blue","green","cyan","orange","brown")
-  filePath <- paste('/Users/Shared/Experiments/',expDir,'/results/R/',sep="")
-  elecList = c('F7','Fz','F8','T7','Cz','T8','P7','Pz','P8')
-  
-  if(pdfQ=='yes'){pdf(file=paste(filePath,'figures/ERP_9elec_',subjListFileID,'.pdf',sep=""))}
-  par(mfrow=c(3,3))
-  for(elec in elecList){
-    plotBasicERP(expDir, expName, subjListFileID, condList, epBeg, epEnd, sampleRate, elec,colScheme)
-    #mtext("μV",side=2,adj=-3, line=1, cex=1, las=1, at=4)  ##mu symbol won't encode correctly in pdf
-  }
-  #mtext(subjListFileID,side=3,adj=.5,cex=1.5)
-  legend(350, -5, condLabelList, col=colScheme[1:length(condList)], lty=c(1,1), merge=TRUE,lwd=1.5,bty="n",cex=.7)
-  if(pdfQ=='yes'){dev.off()}
-}
-
-
-
-
-#This function plots the waveform for a single electrode in R, with option to export to pdf
-#Files needed: 
-##(1)ERP files for each subj/cond exported from Matlab, as described above
-##(2)Subject list text file in 'Experiments/expDir/results/R/' that has one subject number per row
-##(3)To export pdfs, need to create a directory 'Experiments/expDir/results/R/figures/'
-
-#EX: plotERP.1e('MOOSE', 'MOOSE', 'MOOSE_n33', c(1,2,3, 4), c('curly hair (hi-con hi-prob)', 'soaked hair', 'curly leaf (hi-con lo-prob)', 'crunchy leaf'),-100, 798, 500, 6,'yes')
-
-#!!elecLabelList in grandAvgElec should be triple-checked to verify it is correct
-
-plotERP.1e.orig <- function(expDir, expName, subjListFileID, condList, condLabelList,epBeg, epEnd, sampleRate, elec,pdfQ){
-  filePath <- paste('/Users/Shared/Experiments/',expDir,'/results/R/',sep="")
-  fileName <- paste(filePath,subjListFileID, '.txt',sep="")
-  subjList = scan(file=fileName,flush=TRUE)
-  time_vector <- seq(epBeg,epEnd,1000/sampleRate)
-  col.scheme = c("black","red","blue","green","cyan","orange","brown")
-  if(pdfQ=='yes'){pdf(file=paste(filePath,'figures/ERP_',elec,'_',subjListFileID,'.pdf',sep=""))}
-  
-  firstCond <- condList[1]
-  ga_c1 <- grandAvgElec(expDir, expName,subjList,condList[1],elec)
-  plot(time_vector, ga_c1, type="l", ylim=c(12,-6), col=col.scheme[1],ylab="",xlab="",xaxt="n",yaxt="n",lwd=2.5, bty="n")
-  axis(1,labels=FALSE,pos=c(0,0),lwd=1,tcl=-.5,at=seq(epBeg,epEnd+5,100))
-  axis(2,labels=TRUE,pos=c(0,0),las=1,lwd=1,tcl=-.5,at=c(-3,3))
-  mtext(elec,side=2,adj=.5,cex=2,las=1,at=-4)
-  #mtext("μV",side=2,adj=-3, line=1, cex=1, las=1, at=4)  ##mu symbol won't encode correctly in pdf
-  mtext(subjListFileID,side=3,adj=.5,cex=1.5)
-  
-  if(length(condList) > 1){
-    otherConds <-condList[2:length(condList)]    
-    count = 0  
-    for (cond in otherConds){
-      count = count + 1
-      ga_otherCond <- grandAvgElec(expDir,expName,subjList,otherConds[count],elec)
-      lines(time_vector, ga_otherCond, col=col.scheme[count+1],lwd=2)
-    }
-  }
-  
-  legend(-100, 6, condLabelList, col=col.scheme[1:length(condList)], lty=c(1,1), merge=TRUE,lwd=2.5,bty="n")
-  if(pdfQ=='yes'){dev.off()}
-  
 }
